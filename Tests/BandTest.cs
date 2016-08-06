2fbc28b3b4a7ac503648034a720fd541a938c64e
Sherly789 <sherlysh789>
2016-08-05 17:36:45 -0700

    add test to save band to database

--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -34,6 +34,21 @@ namespace BandTracker
       Assert.Equal(firstBand, secondBand);
     }
 
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      //Arrange
+      Band testBand = new Band("The Music");
+
+      //Act
+      testBand.Save();
+      List<Band> result = Band.GetAll();
+      List<Band> testList = new List<Band>{testBand};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
     public void Dispose()
     {
       Band.DeleteAll();
